0b4d4e05c2fb19dba98913e810b41503d71b7ba5
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-04-24 05:49:58 +0800

    Add files via upload

---
+++ Loop Even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Mark Kenneth O. Ferrer DICT 1-3*/
+int main() {
+	int i, n;
+
+    printf("Even numbers from 2 to 200 are: \n", n);
+
+ 
+    for(i=2; i<=200; i++)
+    {
+       
+        if(i%2 == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
+
